df7137f0e1e08dc81d4683ed5497297ea48b8158
yousef alaa <ya123laa@gmail.com>
2021-09-21 11:12:05 -0700

    creat my game

---
+++ Assets/Ball.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    private bool Ispresed = false;
+    public Rigidbody2D rb;
+    public Rigidbody2D Hock;
+    public float releasetime = .15f;
+    public float maxDragdistance = 2f;
+    public GameObject NextBall;
+    void Update()
+    {
+        if (Ispresed)
+        {
+            Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousepos, Hock.position) > maxDragdistance)
+            {
+                rb.position = Hock.position + (mousepos - Hock.position).normalized * maxDragdistance;
+            }
+            else
+            {
+                rb.position = mousepos;
+            }
+            
+        }
+    }
+  
+    void OnMouseDown()
+    {
+        Ispresed = true;
+        rb.isKinematic = true;
+    }
+    void OnMouseUp()
+    {
+        Ispresed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release ()
+    {
+        yield return new WaitForSeconds(releasetime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+        if (NextBall != null)
+        {
+            NextBall.SetActive(true);
+        }
+        else
+        {
+            Enemy.EnemiAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+     
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 4f;
+    public static int EnemiAlive = 0;
+
+    void Start()
+    {
+        EnemiAlive++;
+    }
+  void OnCollisionEnter2D(Collision2D colinf)
+    {
+        if (colinf.relativeVelocity.magnitude > Health)
+        {
+            Die();
+        }
+        
+    }
+    void Die()
+    {
+        EnemiAlive--;
+        if (EnemiAlive <= 0)
+        {
+            Debug.Log("Game Won!");
+        }
+        Destroy(gameObject);
+    }
+}
